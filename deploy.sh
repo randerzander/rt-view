2560684660d5fbb06673d5c80bf29b8828242b0c
Randy Gelhausen <rgelhau@gmail.com>
2015-05-03 15:34:38 -0700

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# Install UI dependencies via bower
+cd src/main/resources/ui
+bower install
+cd ../../../../
+
+# Build, install view, restart ambari-server
+mvn clean package
+sudo rm -rf /var/lib/ambari-server/resources/views/work/rt\{0.1.0\}/
+sudo cp target/*-view.jar /var/lib/ambari-server/resources/views/
+sudo service ambari-server restart
