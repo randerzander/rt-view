bb1e6acd7ba8ff506958206750097ff0bff78657
Randy Gelhausen <rgelhau@gmail.com>
2015-05-05 23:48:16 -0700

    Cleaned up sockets, adjusted web.xml

--- src/main/java/com/github/randerzander/view/Sockets.java
+++ src/main/java/com/github/randerzander/view/Sockets.java
@@ -62,16 +62,10 @@ public class Sockets extends WebSocketServlet {
       props.put("zookeeper.sync.time.ms", "200");
       props.put("auto.commit.interval.ms", "1000");
       consumer = kafka.consumer.Consumer.createJavaConsumerConnector(new ConsumerConfig(props));
-
-      sessions = new HashMap<String, Session>();
-      subscribedTopics = new HashMap<String, String>();
     }
 
-    private HashMap<String, Session> sessions;
-    private HashMap<String, ArrayList<String>> subscriptions;
-    private HashMap<String, String> subscribedTopics;
     @ClientEndpoint
-    @ServerEndpoint(value="/ws/")
+    @ServerEndpoint(value="/sockets/")
     public class EventSocket{
       private Session session;
 
@@ -79,57 +73,42 @@ public class Sockets extends WebSocketServlet {
       public void onWebSocketText(String message){
         String sessionKey = this.session.getId();
         System.out.println("SOCKET got TEXT message: " + message + ", from " + sessionKey);
-        ArrayList<String> sessionSubs;
-        if (subscriptions.containsKey(sessionKey)) sessionSubs = (ArrayList<String>)subscriptions.get(sessionKey);
-        else sessionSubs = new ArrayList<String>();
-        sessionSubs.add(message);
-        subscriptions.put(sessionKey, sessionSubs);
-
-        if (!subscribedTopics.containsKey(message)){
-          subscribedTopics.put(message, null);
-          run(message);
-        }
+        run(this.session, message);
       }
       
       @OnOpen
       public void onWebSocketConnect(Session session){ System.out.println("SOCKET Connected: " + session.getId());
-        sessions.put(session.getId(), session);
         this.session=session;
       }
       @OnClose
-      public void onWebSocketClose(CloseReason reason){ System.out.println("SOCKET Closed: " + reason);
-        sessions.remove(this.session.getId());
-      }
+      public void onWebSocketClose(CloseReason reason){ System.out.println("SOCKET Closed: " + reason); }
       @OnError
       public void onWebSocketError(Throwable cause){ cause.printStackTrace(System.err); }
     }
 
-    public void run(String topic) {
+    public void run(Session session, String topic){
       Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
       topicCountMap.put(topic, new Integer(1));
       Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-      List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
 
       executor = Executors.newFixedThreadPool(1);
-      for (final KafkaStream stream : streams) executor.submit(new Consumer(stream));
+      for (final KafkaStream stream : consumerMap.get(topic)) executor.submit(new Consumer(stream, session));
     }
 
     private class Consumer implements Runnable {
-    //private class Consumer implements Callable {
       private KafkaStream stream;
-      public Consumer(KafkaStream stream) { this.stream = stream; }
+      private Session session;
+      public Consumer(KafkaStream stream, Session session) { this.stream = stream; this.session = session; }
    
       public void run() {
-      //public ArrayList<String> call() {
-        //ArrayList<String> list = new ArrayList<String>();
         ConsumerIterator<byte[], byte[]> it = stream.iterator();
-        while (it.hasNext()){
-          //list.add(new String(it.next().message()));
-
+        while(true){
+          try{
+            while (it.hasNext())
+              session.getBasicRemote().sendText(new String(it.next().message()));
+            Thread.sleep(300);
+          }catch (Exception e) { e.printStackTrace(); return; }
         }
-        //return list;
       }
     }
-
-
 }
